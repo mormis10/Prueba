418e41e78935a9dcb1a9c82f175d720a203f65ca
mormis10 <cerdasignacio5@gmail.com>
2026-09-05 01:02:11 -0600

    oojo

--- app.js
+++ app.js
@@ -6,12 +6,12 @@ function startApplication() {
     console.log(`Server running on port ${config.port}`); 
  
     const USER2 = getUser(1); 
-    const prueba = 200; 
+    const prueba = 500; 
  
     console.log('User:', USER2); 
-    console.log("Ven gózalo ayy vengózao"); 
+    console.log("Hola desde la API"); 
     console.log(prueba); 
-    console.log("Aplicación iniciada correctamente"); 
+    console.log("Conexión con el servidor establecida"); 
 } 
  
 startApplication();
