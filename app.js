4857c4e7345baac7638f484f65338e06030dbbec
mormis10 <cerdasignacio5@gmail.com>
2026-09-05 01:12:44 -0600

    Prueba

--- app.js
+++ app.js
@@ -1,17 +1,16 @@
-const config = require('./config'); 
-const { getUser } = require('./users'); 
-const { getProduct } = require('./products'); 
- 
-function startApplication() { 
-    console.log(`Server running on port ${config.port}`); 
- 
-    const USER2 = getUser(1); 
-    const prueba = 500; 
- 
-    console.log('User:', USER2); 
-    console.log("Hola desde la API"); 
-    console.log(prueba); 
-    console.log("Conexión con el servidor establecida"); 
-} 
- 
+const config = require('./config');
+const { getUser } = require('./users');
+const { getProduct } = require('./products');
+
+function startApplication() {
+    console.log(`Server running on port ${config.port}`);
+
+    const USER2 = getUser(1);
+    const prueba = 999; 
+
+    console.log('User:', USER2);
+    console.log("Esto es compltkfm{kl{rgrpe+gm+ñwlmg");
+    console.log(prueba);
+}
+
 startApplication();
