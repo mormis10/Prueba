e0c5b208ef9654bba66bec317e45f49df9b5e844
mormis10 <cerdasignacio5@gmail.com>
2026-09-05 00:56:34 -0600

    Prueba 2

--- app.js
+++ app.js
@@ -6,10 +6,11 @@ function startApplication() {
     console.log(`Server running on port ${config.port}`);
 
     const USER2 = getUser(1);
-    const product = getProduct(1);
+    const prueba = 200;
 
     console.log('User:', USER2);
-    console.log("Ven bailalo");
+    console.log("Ven gózalo ayy vengózao");
+    console.log(prueba);
 }
 
 startApplication();
